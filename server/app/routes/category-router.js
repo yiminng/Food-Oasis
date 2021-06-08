e6c9f5d3e780a40382e67d5850cfc69b87cf0327
Edwan <edwanhernandez@gmail.com>
2021-06-07 18:19:33 -0700

    Import Category Joi Middleware
    Middleware to validate Category Endpoints

--- server/app/routes/category-router.js
+++ server/app/routes/category-router.js
@@ -3,6 +3,9 @@ const categoryController = require("../controllers/category-controller");
 
 const jwtSession = require("../../middleware/jwt-session");
 
+// eslint-disable-next-line no-unused-vars
+const categoryJoiMiddleware = require("../../middleware/joi/category-controller-validation");
+
 router.get("/", categoryController.getAll);
 router.get("/:id", categoryController.getById);
 router.post(
